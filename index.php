319e2982dd409ef4e25ca3e5dc1afee60fd49b6d
oscarlopezv <oscarlopez_v@hotmail.com>
2023-11-23 12:17:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -131,7 +131,7 @@ a:hover .link-popup {
 <body>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<a href="https://wa.me/593981160259" class="float" target="_blank">
+<a href="https://wa.me/0939908465" class="float" target="_blank">
 <i class="fa fa-whatsapp my-float"></i>
 </a>
     
