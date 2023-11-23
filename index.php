8c46e00ea14c9c9540cc31ba6aa86c2b7be038d5
oscarlopezv <oscarlopez_v@hotmail.com>
2023-11-23 12:15:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -99,7 +99,24 @@ a:hover .link-popup {
 .row tr, .row tr, .row td, .row div, .row span, .row img, .row table{
     background: transparent !important
 }
+.float{
+	position:fixed;
+	width:60px;
+	height:60px;
+	bottom:40px;
+	right:40px;
+	background-color:#25d366;
+	color:#FFF;
+	border-radius:50px;
+	text-align:center;
+  font-size:30px;
+	box-shadow: 2px 2px 3px #999;
+  z-index:100;
+}
 
+.my-float{
+	margin-top:16px;
+}
 </style>    
     
 <!-- JavaScripts -->
@@ -112,7 +129,11 @@ a:hover .link-popup {
     <![endif]-->
 </head>
 <body>
-       
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<a href="https://wa.me/593981160259" class="float" target="_blank">
+<i class="fa fa-whatsapp my-float"></i>
+</a>
     
 <!-- LOADER ===========================================-->
 <div id="loader">
